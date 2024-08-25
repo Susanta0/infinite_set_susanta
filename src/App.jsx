cdb10ed40fa577fa82c39584040f52c27b6012e4
Susanta0 <susanta721467@gmail.com>
2024-08-25 15:58:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Sidebar } from './components/Sidebar'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Sidebar/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Dashboard } from './Dashboard'
+
+export const Main = () => {
+  return (
+    <>
+    <Headers/>
+    <Dashboard/>
+    </>
+  )
+}
